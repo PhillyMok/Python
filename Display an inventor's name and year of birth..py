81de52e40f3340e747be9a53193dd9f728b46177
Philly Mok <67905270+PhillyMok@users.noreply.github.com>
2022-05-13 11:13:17 -0400

    Add files via upload

---
+++ Display an inventor's name and year of birth..py
@@ -0,0 +1,6 @@
+## Display an inventor's name and year of birth.
+firstName = "Philly"
+middleName = ""
+lastName = "Mok"
+yearOfBirth = 1990
+print(firstName, middleName, lastName + ',', yearOfBirth)
