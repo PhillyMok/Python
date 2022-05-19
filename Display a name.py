27f571c10a6db1eaa41861f2d242214f2b8707bc
Philly Mok <67905270+PhillyMok@users.noreply.github.com>
2022-05-18 20:14:26 -0400

    Add files via upload

---
+++ Display a name.py
@@ -0,0 +1,4 @@
+## Display a name.
+name = input("Enter a 2-part name: ")
+L = name.split()
+print("{0:s}, {1:s}".format(L[1], L[0]))
---
+++ Extract the middle name from a three-part name.py
@@ -0,0 +1,4 @@
+## Extract the middle name from a three-part name.
+name = input("Enter a 3-part Name: ")
+L = name.split()
+print("Middle Name:", L[1])
