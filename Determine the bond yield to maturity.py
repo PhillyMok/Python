e701d9b7acfcd00307523da2ceab9c27ec9df94d
Philly Mok <67905270+PhillyMok@users.noreply.github.com>
2022-05-20 19:01:10 -0400

    Add files via upload

---
+++ Determine the bond yield to maturity.py
@@ -0,0 +1,10 @@
+## Determine the bond yield to maturity.
+faceValue = float(input("Enter face value of bond: "))
+couponRate = float(input("Enter coupon interest rate: "))
+interest = faceValue * couponRate
+marketPrice = float(input("Enter current market price: "))
+yrsUntilMaturity = float(input("Enter years until maturity: "))
+a = (faceValue - marketPrice) / yrsUntilMaturity
+b = (faceValue + marketPrice) / 2
+ytm = (interest + a) / b
+print("Approximate YTM: {0:.2%}".format(ytm))
