ad7b3c190122019152cebdd288e45e3764c52948
Philly Mok <67905270+PhillyMok@users.noreply.github.com>
2022-06-20 22:56:21 -0400

    Add files via upload

---
+++ Count the number of vowels in a phrase.py
@@ -0,0 +1,8 @@
+## Count the number of vowels in a phrase.
+total = 0
+phrase = input("Enter a phrase: ")
+phrase = phrase.lower()
+for ch in phrase:
+    if ch in "aeiou":
+        total += 1
+print("The phrase contains", total, "vowels.")
